191fb9122fee4dcc8a73849a7d04a2c910599ae4
Tom Kopp <witzbould@live.com>
2018-06-28 14:39:19 +0200

    add debounce

---
+++ debounce.js
@@ -0,0 +1,23 @@
+export const debounce = (func, wait, immediate) => {
+	if (typeof func !== 'function') {
+		throw new TypeError('Expected a function');
+	}
+
+	let timeout;
+
+	return function debounced(...args) {
+		const later = () => {
+			timeout = null;
+			if (!immediate) {
+				func.apply(this, args);
+			}
+		};
+		const callNow = immediate && !timeout;
+
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) {
+			func.apply(this, args);
+		}
+	};
+};
