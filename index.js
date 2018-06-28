1bcbcaa261ec070e56af3a3ab783e4fea4f411a9
Tom Kopp <witzbould@live.com>
2018-06-28 14:43:20 +0200

    add module entrypoint

---
+++ index.js
@@ -0,0 +1,14 @@
+import { jsonParse, jsonParseSync } from 'jsonParse.js';
+import clampValue from 'clampValue.js';
+import debounce from 'debounce.js';
+import mapValueFromInputToOutputRange from 'mapValueFromInputToOutputRange.js';
+import throttle from 'throttle.js';
+
+export default {
+	clampValue
+	, debounce
+	, jsonParse
+	, jsonParseSync
+	, mapValueFromInputToOutputRange
+	, throttle
+};
