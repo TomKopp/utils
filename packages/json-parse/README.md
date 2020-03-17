3525b170acb6640132b22ec3d748eb0543be8ac3
Tom Kopp <witzbould@outlook.com>
2020-03-17 21:56:28 +0100

    move jsonParse to its own package

---
+++ packages/json-parse/README.md
@@ -0,0 +1,4 @@
+# JsonParse
+
+Wrapper around Json.parse() to catch any errors and return a Promise.\
+The synchronous version returns the Object or an Error object.
--- packages/json-parse/index.js
+++ packages/json-parse/index.js
@@ -1,3 +1,9 @@
+/**
+ * Asynchronous wrapper around JSON.parse to catch any errors
+ *
+ * @param  {string} data The json string
+ * @return {Promise} Promise, that fulfills with the data or rejects on error
+ */
 export const jsonParse = (data) => new Promise((resolve, reject) => {
 	try {
 		resolve(JSON.parse(data));
@@ -7,6 +13,12 @@ export const jsonParse = (data) => new Promise((resolve, reject) => {
 	}
 });
 
+/**
+ * Synchronous wrapper around JSON.parse to catch any errors
+ *
+ * @param  {string} data The json string
+ * @return {(object|SyntaxError)} Returns the parsed object on success or syntax error on failure
+ */
 export const jsonParseSync = (data) => {
 	let ret;
 
