0787295a2b0a8834d9f5904667d1ba9edf0e8ae9
Tom Kopp <tom.kopp@zalari.de>
2024-02-03 16:19:27 +0100

    feat(json-parse)!: :boom: rework jsonParseSync to return obj with error and data prop

--- packages/json-parse/index.js
+++ packages/json-parse/index.js
@@ -1,6 +1,7 @@
 /**
  * Asynchronous wrapper around JSON.parse to catch any errors
  *
+ * @exports
  * @param  {string} data The json string
  * @return {Promise} Promise, that fulfills with the data or rejects on error
  */
@@ -16,17 +17,22 @@ export const jsonParse = (data) => new Promise((resolve, reject) => {
 /**
  * Synchronous wrapper around JSON.parse to catch any errors
  *
+ * @typedef {object} jsonParseSyncReturn
+ * @property {object} data The parsed object
+ * @property {SyntaxError} error The syntax error if parsing failed, otherwise undefined
+ *
+ * @exports
  * @param  {string} data The json string
- * @return {(object|SyntaxError)} Returns the parsed object on success or syntax error on failure
+ * @return {jsonParseSyncReturn} Returns an object with an error property if parsing failed, otherwise data is populated
  */
 export const jsonParseSync = (data) => {
-	let ret;
+	let ret = {};
 
 	try {
-		ret = JSON.parse(data);
+		ret.data = JSON.parse(data);
 	} catch (error) {
 		// @value {SyntaxError} error json parse throws SyntaxError
-		ret = error;
+		ret.error = error;
 	}
 
 	return ret;
