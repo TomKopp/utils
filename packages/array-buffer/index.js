ec78f373ad0e91bdea21a8ba4e11005df46a5858
Tom Kopp <tom.kopp@zalari.de>
2024-09-20 13:18:55 +0200

    feat(array-buffer): :sparkles: add ArrayBuffer <-> (binary)String conversion

--- packages/array-buffer/index.js
+++ packages/array-buffer/index.js
@@ -1,10 +1,26 @@
 /**
+ * Converts an ArrayBuffer to a binary string that can be used with btoa.
+ *
+ * @param {ArrayBuffer} buffer - The ArrayBuffer to convert.
+ * @return {string} The resulting binary string.
+ */
+export const arrayBufferToBinaryString = (buffer) => Array.from(new Uint8Array(buffer), (x) => String.fromCodePoint(x)).join('');
+
+/**
+ * Converts a binary string to an ArrayBuffer that can be used with atob.
+ *
+ * @param {string} binaryString - The binary string to convert.
+ * @return {ArrayBuffer} The resulting ArrayBuffer.
+ */
+export const binaryStringToArrayBuffer = (binaryString) => Uint8Array.from(binaryString, (m) => m.codePointAt(0)).buffer;
+
+/**
  * Converts an ArrayBuffer to a Base64 encoded string.
  *
  * @param {ArrayBuffer} buffer - The ArrayBuffer to convert.
  * @return {string} The Base64 encoded string.
  */
-export const arrayBufferToBase64 = (buffer) => btoa(Array.from(new Uint8Array(buffer), (x) => String.fromCodePoint(x)).join(''));
+export const arrayBufferToBase64 = (buffer) => btoa(arrayBufferToBinaryString(buffer));
 
 /**
  * Converts a Base64 encoded string to an ArrayBuffer.
@@ -12,4 +28,4 @@ export const arrayBufferToBase64 = (buffer) => btoa(Array.from(new Uint8Array(bu
  * @param {string} base64 - The Base64 encoded string to convert.
  * @return {ArrayBuffer} The resulting ArrayBuffer.
  */
-export const base64ToArrayBuffer = (base64) => Uint8Array.from(atob(base64), (m) => m.codePointAt(0)).buffer;
+export const base64ToArrayBuffer = (base64) => binaryStringToArrayBuffer(atob(base64));
